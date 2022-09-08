5a0af9ff77f5f27ec5be0fc0ed6ce04e10e7f9c0
Sukrit pipitkul <Surit.pi@ku.th>
2022-09-08 19:48:03 +0700

    second commit

---
+++ config_api_control_and_dht22.py
@@ -0,0 +1,7 @@
+WIFI_SSID = 'AndroidAP'
+WIFI_PASSWORD = '12345prem'
+
+WEBHOOK_URL = 'https://api.thingspeak.com/update?api_key=AYO0I4JS17QL5C60&field1={temperature}&field2={humidity}&field3={relay}'
+WEBHOOK_URL1 = 'https://api.thingspeak.com/channels/1821481/feeds.json?api_key=X3Y29QCVZLBL1J82&results=2'
+WEBHOOK_URL2 = 'https://api.thingspeak.com/channels/1833628/feeds.json?api_key=C2UWYL5UJQ5VB1N2&results=2'
+                
